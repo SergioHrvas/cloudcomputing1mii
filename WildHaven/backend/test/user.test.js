d0bb210e7349f43a8cb6b37f164e635e7809a2e8
SergioHrvas <sergiohervas18@gmail.com>
2024-11-06 22:46:35 +0100

    Estructura del test

---
+++ WildHaven/backend/test/user.test.js
@@ -0,0 +1,10 @@
+const User = require('../controllers/user.js')
+const chai = require('chai');
+
+describe('Testing', function() {
+    it('1.', function(done) {
+    chai.expect(2).to.equal(2);
+    done();
+});
+
+})
