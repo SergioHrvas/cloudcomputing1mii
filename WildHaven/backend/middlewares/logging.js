8bc32994a8d8516d6de598b47e6f6f62e9d026f1
SergioHrvas <sergiohervas18@gmail.com>
2024-11-15 18:37:54 +0100

    Mejorar logs
    Mejorar sistema de logs para que devuelvan más información - closes #64

--- WildHaven/backend/middlewares/logging.js
+++ WildHaven/backend/middlewares/logging.js
@@ -4,12 +4,32 @@ const logger = require('../logger.js');
 const requestLogger = (req, res, next) => {
   const start = Date.now();
 
+  // Extraer detalles adicionales
+  const { method, originalUrl, headers, body } = req;
+  const { statusCode } = res;
+  const ip = req.ip || req.connection.remoteAddress; // Dirección IP del cliente
+  const userAgent = headers['user-agent']; // User-Agent de la solicitud
+  
+
+  // Verificar si hay un usuario autenticado (si tienes autenticación implementada)
+  const user = req.user ? req.user.username : 'Unauthenticated';
+
   res.on('finish', () => {
-    const { method, originalUrl } = req;
-    const { statusCode } = res;
     const duration = Date.now() - start;
 
-    logger.info(`${method} ${originalUrl} ${statusCode} - ${duration}ms`);
+    // Puedes almacenar más información aquí dependiendo de lo que necesites
+    logger.info({
+      message: `${method} ${originalUrl} ${statusCode} - ${duration}ms (${ip})`,
+      method,
+      url: originalUrl,
+      statusCode,
+      duration,
+      ip,
+      userAgent,
+      user,
+      body: method === 'POST' || method === 'PUT' ? JSON.stringify(body) : undefined, // Solo guarda el cuerpo para POST/PUT
+      timestamp: new Date().toISOString()
+    });
   });
 
   next();
