7517321a098d6188b52bcecf39d9f8c4bcc101ef
SergioHrvas <sergiohervas18@gmail.com>
2025-01-17 17:47:43 +0100

    Intentando desplegar en GC

--- WildHaven/backend/app.js
+++ WildHaven/backend/app.js
@@ -12,7 +12,20 @@ var app = express();
 
 const cors = require('cors');
 // Configurar CORS para aceptar solicitudes desde tu frontend Angular
-app.use(cors()); 
+// Set up CORS
+app.use(cors({
+    origin: true, // "true" will copy the domain of the request back
+                  // to the reply. If you need more control than this
+                  // use a function.
+
+    credentials: true, // This MUST be "true" if your endpoint is
+                       // authenticated via either a session cookie
+                       // or Authorization header. Otherwise the
+                       // browser will block the response.
+
+    methods: 'POST,GET,PUT,OPTIONS,DELETE' // Make sure you're not blocking
+                                           // pre-flight OPTIONS requests
+}));
 
 // Aumentar el límite del cuerpo de la solicitud
 app.use(express.json({ limit: '50mb' })); // Para JSON
