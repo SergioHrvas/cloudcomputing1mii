e322f6d9ee3f3c43c2646b291f92a02723a67dda
SergioHrvas <sergiohervas18@gmail.com>
2025-01-18 09:38:53 +0100

    Entorno de producción

--- WildHaven/frontend/WildHaven/src/app/services/global.ts
+++ WildHaven/frontend/WildHaven/src/app/services/global.ts
@@ -1,17 +1,6 @@
-let apiUrl: string;
-let uploadsUrl: string;
+import { environment } from '../../environments/environment';
 
-if (window.location.hostname === 'localhost') {
-    // Si estamos en desarrollo
-    apiUrl = "http://localhost:3800/api/";
-    uploadsUrl = "http://localhost:3800/uploads/";
-} else {
-    // Si estamos en producción (Google Cloud)
-    apiUrl = "http://34.175.69.125:3800/api/";
-    uploadsUrl = "http://34.175.69.125:3800/uploads/";
-}
-
-export const GLOBAL = {
-    url: apiUrl,
-    urlUploads: uploadsUrl
+export var GLOBAL = {
+  url: environment.apiUrl,
+  urlUploads: environment.uploadsUrl
 };
---
+++ WildHaven/frontend/WildHaven/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,   // Indica que es un entorno de desarrollo
+    apiUrl: 'http://localhost:3800/api/',  // URL para desarrollo
+    uploadsUrl: 'http://localhost:3800/uploads/'
+  };
