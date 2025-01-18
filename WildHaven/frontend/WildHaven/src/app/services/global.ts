a074e0feff8b90c321c1325c086884e2292394bd
SergioHrvas <sergiohervas18@gmail.com>
2025-01-18 09:04:00 +0100

    automatizacion despliegue

--- WildHaven/frontend/WildHaven/src/app/services/global.ts
+++ WildHaven/frontend/WildHaven/src/app/services/global.ts
@@ -1,4 +1,17 @@
-export var GLOBAL = {
-    url: "http://localhost:3800/api/",
-    urlUploads: "http://localhost:3800/uploads/"
+let apiUrl: string;
+let uploadsUrl: string;
+
+if (window.location.hostname === 'localhost') {
+    // Si estamos en desarrollo
+    apiUrl = "http://localhost:3800/api/";
+    uploadsUrl = "http://localhost:3800/uploads/";
+} else {
+    // Si estamos en producción (Google Cloud)
+    apiUrl = "http://34.175.69.125:3800/api/";
+    uploadsUrl = "http://34.175.69.125:3800/uploads/";
 }
+
+export const GLOBAL = {
+    url: apiUrl,
+    urlUploads: uploadsUrl
+};
