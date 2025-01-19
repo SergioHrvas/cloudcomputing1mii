d2810c92be54a5a71f1fff249b046d8138c8028e
SergioHrvas <sergiohervas18@gmail.com>
2025-01-19 22:12:32 +0100

    cambio en la IP

--- WildHaven/frontend/WildHaven/src/environments/environment.prod.ts
+++ WildHaven/frontend/WildHaven/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,    // Indica que es un entorno de producción
-    apiUrl: 'http://34.175.69.125:3800/api/',  // URL para producción
-    uploadsUrl: 'http://34.175.69.125:3800/uploads/'
+    apiUrl: 'http://34.175.235.139:3800/api/',  // URL para producción
+    uploadsUrl: 'http://34.175.235.139:3800/uploads/'
   };
   
